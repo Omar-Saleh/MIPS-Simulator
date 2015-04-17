e711b821878fb68c070d6257bff3dc6d7d147d15
Omar Saleh <omarsaleh0106@gmail.com>
2015-04-17 04:20:00 +0200

    Added function codes to R format instructions no support for I or J format instructions yet

--- InstructionMem.py
+++
@@ -1,8 +0,0 @@
-class InstructionMem(object):
-	"""PC is the instruction address
-	   instruction is the instruction being executed before it is decoded into binary	
-	"""
-	def __init__(self, PC):
-		self.PC = PC
-		self.instruction = arr[PC]
-		
--- gen.py
+++
@@ -1,3 +0,0 @@
-with open("gen.txt" , "a") as t:
-	for i in range(0 , 32):
-		t.write("\"" + bin(i).replace("0b" , "") + '\"\n')
